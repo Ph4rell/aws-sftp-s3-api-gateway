95acc496aa75e9dbb4306f063b2ea381a16a156e
Pierre <pierre.poree@d2si.io>
2020-03-18 18:45:39 +0100

    add policy for sftp server

--- iam.tf
+++ iam.tf
@@ -18,3 +18,35 @@ data "aws_iam_policy_document" "transfer_assume_role_policy" {
     }
   }
 }
+
+resource "aws_iam_policy" "transfer_policy" {
+  name = "TransferCanInvokeApi"
+  policy = data.aws_iam_policy_document.policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "attachment" {
+  policy_arn = aws_iam_policy.transfer_policy.arn
+  role = aws_iam_role.transfer_role.id
+}
+
+data "aws_iam_policy_document" "policy" {
+  version = "2012-10-17"
+
+  statement {
+    sid = "InvokeAPI"
+    actions = ["execute-api:Invoke"]
+    effect = "Allow"
+    resources = [
+      "${aws_api_gateway_stage.stage.execution_arn}"
+    ]
+  }
+
+  statement {
+    sid = "TransferCanReadThisApi"
+    actions = ["apigateway:GET"]
+    effect = "Allow"
+    resources = [
+      "*"
+    ]
+  }
+}
