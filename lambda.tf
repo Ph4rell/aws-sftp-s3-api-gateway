6eb819620582b9f1afbc503be3535043ef8205c6
Pierre <pierre.poree@d2si.io>
2020-03-19 16:25:49 +0100

    add lambda permission invoke from api

--- lambda.tf
+++ lambda.tf
@@ -6,33 +6,44 @@ data "archive_file" "zip" {
 
 resource "aws_lambda_function" "lambda" {
   filename      = "lambda.zip"
-  function_name = "test"
-  role          = aws_iam_role.lambda_assume_role.arn
+  function_name = var.lambda_name
+  role          = aws_iam_role.lambda_role.arn
   handler       = "lambda.lambda_handler"
   runtime       = "python3.6"
   source_code_hash = filebase64sha256("lambda.zip")
 }
 
-resource "aws_iam_role" "lambda_assume_role" {
-  name = "assume-role-lambda-test"
+resource "aws_lambda_permission" "lambda_permission" {
+  statement_id  = "AllowAPIInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = var.lambda_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*/*"
+}
 
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
+resource "aws_iam_role" "lambda_role" {
+  name 		          = "LambdRole"
+  description         = "Lambda role"
+  assume_role_policy  = data.aws_iam_policy_document.lambda_assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "lambda_assume_role_policy" {
+  statement {
+    sid = "AssumeRole"
+    actions = [
+      "sts:AssumeRole"
+    ]
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = [
+        "lambda.amazonaws.com"
+      ]
     }
-  ]
-}
-POLICY
+  }
 }
 
-data "aws_iam_policy" "ManagedPolicy" {
-    arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+resource "aws_iam_role_policy_attachment" "ManagedPolicy_attachment" {
+  role = aws_iam_role.lambda_role.id
+  policy_arn = data.aws_iam_policy.ManagedPolicy.arn
 }
