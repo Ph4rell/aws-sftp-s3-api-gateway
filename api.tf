03f6c200f03768ba6fb26957e81fa1f23d605af2
Pierre <pierre.poree@d2si.io>
2020-03-19 15:56:49 +0100

    modify integration type to AWS

--- api.tf
+++ api.tf
@@ -11,7 +11,7 @@ resource "aws_api_gateway_deployment" "deploy" {
 
   depends_on = [
     aws_api_gateway_integration.integration,
-    aws_api_gateway_integration_response.integration_response
+    aws_api_gateway_integration_response.integration_response,
   ]
 }
 
@@ -46,7 +46,7 @@ resource "aws_api_gateway_resource" "config" {
   parent_id   = aws_api_gateway_resource.username.id
   path_part   = "config"
 }
-resource "aws_api_gateway_method" "method" {
+resource "aws_api_gateway_method" "get_method" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   resource_id   = aws_api_gateway_resource.config.id
   http_method   = "GET"
@@ -54,29 +54,31 @@ resource "aws_api_gateway_method" "method" {
   request_parameters = {
     "method.request.querystring.serverId" = true,
     "method.request.querystring.username" = true,
-    "method.request.header.Password" = true
+    "method.request.header.Password" = false
   }
 }
 
 resource "aws_api_gateway_integration" "integration" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
   resource_id             = aws_api_gateway_resource.config.id
-  http_method             = aws_api_gateway_method.method.http_method
+  http_method             = aws_api_gateway_method.get_method.http_method
   integration_http_method = "POST"
-  type                    = "AWS_PROXY"
+  type                    = "AWS"
   uri                     = aws_lambda_function.lambda.invoke_arn
 }
 
 resource "aws_api_gateway_integration_response" "integration_response" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   resource_id = aws_api_gateway_resource.config.id
-  http_method = aws_api_gateway_method.method.http_method
-  status_code = aws_api_gateway_method_response.response_200.status_code
+  http_method = aws_api_gateway_method.get_method.http_method
+  status_code = "200"
 }
 
 resource "aws_api_gateway_method_response" "response_200" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   resource_id = aws_api_gateway_resource.config.id
-  http_method = aws_api_gateway_method.method.http_method
+  http_method = aws_api_gateway_method.get_method.http_method
   status_code = "200"
 }
+
+
