292b343b740b58578473c22bcc63a6e4d68c7038
Pierre <pierre.poree@d2si.io>
2020-03-19 12:28:20 +0100

    change invocation role

--- transfer.tf
+++ transfer.tf
@@ -1,6 +1,6 @@
 resource "aws_transfer_server" "server" {
   identity_provider_type = "API_GATEWAY"
   endpoint_type = "PUBLIC"
-  invocation_role = aws_iam_role.transfer_role.arn
+  invocation_role = aws_iam_role.transfer_IdP_role.arn
   url = aws_api_gateway_stage.stage.invoke_url
 }
