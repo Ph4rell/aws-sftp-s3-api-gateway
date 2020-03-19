2036ba02cc9d9e249714bb14578e61cda92d6458
Pierre <pierre.poree@d2si.io>
2020-03-19 17:11:26 +0100

    add cloudwatch policy for API and SFTP server

--- iam.tf
+++ iam.tf
@@ -57,9 +57,9 @@ data "aws_iam_policy_document" "transfer_IdP_document" {
     resources = [aws_iam_role.TransferSFTPS3AccessRole.arn]
   }
 }
-#############################
-# SFTP Access Bucket S3 Role
-#############################
+#########################################
+# SFTP Access Bucket S3 & Cloudwatch Role
+#########################################
 resource "aws_iam_role" "TransferSFTPS3AccessRole" {
   name 		          = "TransferSFTPS3AccessRole"
   description         = "TransferSFTPS3AccessRole"
@@ -119,6 +119,73 @@ data "aws_iam_policy_document" "TransferSFTPS3Access_document" {
     effect = "Allow"
     resources = [
       "${aws_s3_bucket.bucket.arn}"
+    ]
+  }
+
+  statement {
+    sid = "CloudWatchLogs"
+    actions = [
+      "logs:PutLogEvents",
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream"
+    ]
+    effect = "Allow"
+    resources = [
+      "*"
+    ]
+  }
+}
+
+#############################
+# API Cloudwatch Role
+#############################
+resource "aws_iam_role" "ApiGatewayLogsRole" {
+  name 		          = "ApiGatewayLogsRole"
+  description         = "ApiGatewayLogsRole"
+  assume_role_policy  = data.aws_iam_policy_document.ApiGatewayLogsRole_assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "ApiGatewayLogsRole_assume_role_policy" {
+  statement {
+    sid = "AssumeRole"
+    actions = [
+      "sts:AssumeRole"
+    ]
+    principals {
+      type        = "Service"
+      identifiers = [
+        "apigateway.amazonaws.com"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_policy" "ApiGatewayLogsPolicy" {
+  name = "ApiGatewayLogsPolicy"
+  policy = data.aws_iam_policy_document.ApiGatewayLogsPolicy_document.json
+}
+
+resource "aws_iam_role_policy_attachment" "ApiGatewayLogs_attachment" {
+  policy_arn = aws_iam_policy.ApiGatewayLogsPolicy.arn
+  role = aws_iam_role.ApiGatewayLogsRole.id
+}
+
+data "aws_iam_policy_document" "ApiGatewayLogsPolicy_document" {
+  version = "2012-10-17"
+  statement {
+    sid = "CloudWatchLogs"
+    actions = [
+      "logs:PutLogEvents",
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:DescribeLogGroups",
+      "logs:DescribeLogStreams",
+      "logs:GetLogEvents",
+      "logs:FilterLogEvents"
+    ]
+    effect = "Allow"
+    resources = [
+      "*"
     ]
   }
 }
