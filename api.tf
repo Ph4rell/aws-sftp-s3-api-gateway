f2e16cc006a27c97d785d7c5a1ff2ff9a9493633
Pierre <pierre.poree@d2si.io>
2020-03-20 17:37:11 +0100

    delete useless request parameters

--- api.tf
+++ api.tf
@@ -70,8 +70,6 @@ resource "aws_api_gateway_method" "get_method" {
   http_method   = "GET"
   authorization = "AWS_IAM"
   request_parameters = {
-    # "method.request.querystring.ServerId" = true,
-    # "method.request.querystring.UserName" = true,
     "method.request.header.Password" = false
   }
 }
