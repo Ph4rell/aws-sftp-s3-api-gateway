a1922b7ff40b3b6f2a3c36217650664a0c6e8105
Pierre <pierre.poree@d2si.io>
2020-03-23 15:58:44 +0100

    add s3 encryption and policy to block public access

--- s3.tf
+++ s3.tf
@@ -1,4 +1,20 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = var.mybucket
   acl    = "private"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "default" {
+  bucket                  = aws_s3_bucket.bucket.id
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  restrict_public_buckets = true
 }
