f4094ee579b73778aa6cdffe616284ed355b63bb
Pierre <pierre.poree@d2si.io>
2020-03-23 16:24:37 +0100

    fix some terraform errors

--- lambda.tf
+++ lambda.tf
@@ -11,7 +11,7 @@ resource "aws_lambda_function" "lambda" {
 resource "aws_lambda_permission" "lambda_permission" {
   statement_id  = "AllowAPIInvoke"
   action        = "lambda:InvokeFunction"
-  function_name = var.lambda_name
+  function_name = aws_lambda_function.lambda.function_name
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*/*"
 
